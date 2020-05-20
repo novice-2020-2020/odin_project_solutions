bf4c49ed2dc0ca1a43fa974e90ff0a16b51589d3
sm <43108168+novice-2020-2020@users.noreply.github.com>
2020-05-20 23:54:28 +0530

    Update stock.rb

--- stock.rb
+++ stock.rb
@@ -1,7 +1,7 @@
 prices = [17,3,6,9,15,8,6,1,10]
 
-def min_hash_val(hash)
-  hash.min_by{|k,v| k}
+def max_hash_val(hash)
+  hash.max_by{|k,v| k}
 end
 
 def stock_pick(dict)
@@ -10,15 +10,16 @@ def stock_pick(dict)
     price_buy = dict[i]
     for j in (i..dict.size-1) do
       price_sell = dict[j]
-      diff = price_buy-price_sell
-      price_pairs[diff] = Array([price_buy,price_sell, price_buy-price_sell])
+      diff = price_sell - price_buy
+      price_pairs[diff] = Array([price_buy,price_sell, diff])
     end
     
   end
-  final_pair = min_hash_val(price_pairs)
+  final_pair = max_hash_val(price_pairs)
   final_buy_price = final_pair[1][0]
   final_sell_price = final_pair[1][1]
-    puts "buy at ".concat(final_buy_price.to_s).concat(" and sell at ").concat(final_sell_price.to_s).concat(" for a profit of ").concat((final_sell_price - final_buy_price).to_s)
+  diff = final_sell_price - final_buy_price
+    puts "buy at ".concat(final_buy_price.to_s).concat(" and sell at ").concat(final_sell_price.to_s).concat(" for a profit of ").concat((diff).to_s)
 end
 
 stock_pick(prices)
